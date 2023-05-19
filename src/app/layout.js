bcb9fe15e3957c62c7b252582e3d30e48c4111d7
windbelike <1103031078@qq.com>
2023-05-20 01:42:04 +0800

    font-mono

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout ({ children }) {
   return (
     <html lang="en">
-      <body className='font-sans'>{children}</body>
+      <body className='font-mono'>{children}</body>
     </html>
   )
 }
